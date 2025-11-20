e7893bb390d8e659e01fc33e46e22d26d8ba44fb
Elias <eliasbrockmann@git.seng.uvic.ca>
2025-11-20 01:12:25 -0800

    added basic timer, some commenting

--- chick.py
+++ chick.py
@@ -5,6 +5,7 @@ import os
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 data_dir = os.path.join(main_dir, "data")
 
+# Image loading -- should not be here, put in new file if possible?
 def load_image(name, colorkey=None, scale=1):
     fullname = os.path.join(data_dir, name)
     image = pg.image.load(fullname)
@@ -20,8 +21,8 @@ def load_image(name, colorkey=None, scale=1):
         image.set_colorkey(colorkey, pg.RLEACCEL)
     return image, image.get_rect()
 
-class Chick(pg.sprite.Sprite):
 
+class Chick(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
         #super().__init__()
@@ -34,27 +35,34 @@ class Chick(pg.sprite.Sprite):
         self.movedir = 7
         print(self.movedir)
 
-
+    # Update Sprite, move if alive
     def update(self):
         #print("attempting update" + str(self.alive))
         if self.alive:
-            if np.random.rand() < 0.05:
-                self.movedir = np.random.rand() * (2 * np.pi + 2) 
+            if np.random.rand() < 0.01:
+                self.movedir = np.random.rand() * (2 * np.pi + 5) 
                 #print("attempting move")
             if self.movedir < (2 * np.pi):
                 self.move()
     
+    # Walk
     def move(self):
-        newpos = self.rect.move((np.cos(self.movedir) * 5),np.sin(self.movedir) * 5)
+        newpos = self.rect.move((np.cos(self.movedir) * 3),np.sin(self.movedir) * 3)
         if not self.area.contains(newpos):
             if self.rect.left <= self.area.left or self.rect.right >= self.area.right:
                 self.movedir = np.pi - self.movedir
-                print("reverse" + str(self.movedir))
+                #print("reverse" + str(self.movedir))
                 #self.image = pg.transform.flip(self.image, True, False)
             if self.rect.top <= self.area.top or self.rect.bottom >= self.area.bottom:
                 self.movedir = 2 * np.pi - self.movedir
-                print("reverse")
+                #print("reverse")
             newpos = self.rect.move((np.cos(self.movedir)),np.sin(self.movedir))
         self.rect = newpos
+
+    # Kill the chick
+    def kill(self):
+        self.alive = False
+
+        #update sprite
 
     
--- chicks.py
+++ chicks.py
@@ -8,20 +8,20 @@ from chick import Chick
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 data_dir = os.path.join(main_dir, "data")
 
-def load_image(name, colorkey=None, scale=1):
-    fullname = os.path.join(data_dir, name)
-    image = pg.image.load(fullname)
-    image = image.convert()
+# def load_image(name, colorkey=None, scale=1):
+#     fullname = os.path.join(data_dir, name)
+#     image = pg.image.load(fullname)
+#     image = image.convert()
 
-    size = image.get_size()
-    size = (size[0] * scale, size[1] * scale)
-    image = pg.transform.scale(image, size)
+#     size = image.get_size()
+#     size = (size[0] * scale, size[1] * scale)
+#     image = pg.transform.scale(image, size)
 
-    if colorkey is not None:
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey, pg.RLEACCEL)
-    return image, image.get_rect()
+#     if colorkey is not None:
+#         if colorkey == -1:
+#             colorkey = image.get_at((0, 0))
+#         image.set_colorkey(colorkey, pg.RLEACCEL)
+#     return image, image.get_rect()
 
 def load_sound(name):
     class NoneSound:
@@ -37,10 +37,19 @@ def load_sound(name):
     return sound
 
 def main():
+    # Basic Window Initialization
     pg.init()
     screen = pg.display.set_mode((1280, 720), pg.SCALED)
     pg.display.set_caption("Pomodoro Chicks")
     pg.mouse.set_visible(True)
+
+    isPlayTime = True
+
+    # Reusable timer variables (constants in seconds)
+    STUDY_TIME = 60
+    PLAY_TIME = 20
+    timer_display = {"minutes": 1, "seconds":0}
+
 
     # Create The Background
     background = pg.Surface(screen.get_size())
@@ -50,23 +59,50 @@ def main():
     screen.blit(background, (0,0))
     pg.display.flip()
 
+    # Sprites / text
     chick = Chick()
     allsprites = pg.sprite.RenderPlain(chick)
-    clock = pg.time.Clock()
 
+    font = pg.font.Font(None,50)
+
+    # Start timer
+    clock = pg.time.Clock()
+    start_time = pg.time.get_ticks()
+
+    # Main loop condtion
     going = True
 
+    #Main loop
     while going:
+        # Maximum FPS
         clock.tick(60)
 
+        # Run events
         for event in pg.event.get():
+            # If 'X' buttom pressed
             if event.type == pg.QUIT:
                 going = False
         
+        if isPlayTime:
+            countdown_max = STUDY_TIME
+
+        # Update timer variables
+        seconds_left = int(countdown_max - (pg.time.get_ticks() - start_time) / 1000)
+        timer_display["minutes"] = int(seconds_left / 60)
+        timer_display["seconds"] = seconds_left % 60 
+
+
+        if seconds_left <= 0:
+            print("break time")
+
+        timer_text = font.render(f"Time remaining: {timer_display['minutes']}:{"0" if timer_display['seconds'] < 10 else ""}{timer_display['seconds']}", True, (255, 255, 255))
+
+        # Repaint and update
         allsprites.update()
 
         screen.blit(background, (0,0))
         allsprites.draw(screen)
+        screen.blit(timer_text, (300, 250))
         pg.display.flip()
     pg.quit()
 
